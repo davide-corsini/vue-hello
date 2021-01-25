f9ca0bf0effe3d54391d7dc23831d46394a62d75
davide <davidecorsinimilano@gmail.com>
2021-01-25 22:46:21 +0100

    Push end

--- js/script.js
+++ js/script.js
@@ -4,57 +4,63 @@
 let move = new Vue({
     el: '#container',
     data: {
+        quadrato: false,
         message: false,
-        src: ''
+        src: '',
+        titolo: 'prova'
     },
     methods: {
         addText() {
             this.message = !this.message;
+            this.quadrato = !this.quadrato;
         },
         addImg() {
-
             (this.src == '') ? this.src += 'https://i.ytimg.com/vi/gzc80ics6sI/maxresdefault.jpg' : this.src = '';
-            
         }
+
     }
 })
 
+        let topo = document.getElementById('topo');
+        console.log(topo);
+        const movimento = 10; //questo 10rappresenta i miei px
+        console.log(movimento);
 
 
-    let topo = document.getElementById('topo');
-    console.log(topo);
-    const movimento = 10; //questo 10rappresenta i miei px
-    console.log(movimento);
-    
-
-    //*Questo me lo legge e funziona
-    //fase di partenza
-    window.addEventListener('load', () => {
-        topo.style.position = 'absolute';
-        topo.style.right = 10+'px';//qui é da dove parte il toporagno asse delle x
-        topo.style.bottom = 30 + 'px';//qui stessa cosa ma in altezza sse delle y
-    });
+        //*Questo me lo legge e funziona
+        //fase di partenza
+        window.addEventListener('load', () => {
+            topo.style.position = 'absolute';
+            topo.style.right = 10 + 'px';//qui é da dove parte il toporagno asse delle x
+            topo.style.bottom = 30 + 'px';//qui stessa cosa ma in altezza sse delle y
+        });
 
 
-    //qui gli dico che al click del bottone puoi muovere le frecce ma non va 
-    
-    window.addEventListener('keyup', (speed) => {
+        //qui gli dico che al click del bottone puoi muovere le frecce ma non va 
+
+        window.addEventListener('keydown', (speed) => {
+
+            switch (speed.key) {
+                case 'ArrowRight':
+                    topo.style.right = parseInt(topo.style.right) - movimento + 'px';
+                    break;
+                case 'ArrowLeft':
+                    topo.style.right = parseInt(topo.style.right) + movimento + 'px';
+                    break;
+                case 'ArrowDown':
+                    topo.style.bottom = parseInt(topo.style.bottom) - movimento + 'px';
+                    break;
+                case 'ArrowUp':
+                    topo.style.bottom = parseInt(topo.style.bottom) + movimento + 'px';
+                    break;
+            }
+        });
+
+
         
-        switch(speed.key){
-            case 'ArrowLeft':
-                topo.style.left = parseInt(topo.style.left) - movimento + 'px';
-                break;
-            case 'ArrowRight':
-                topo.style.left = parseInt(topo.style.left) + movimento + 'px';
-                break;
-            case 'ArrowUp':
-                topo.style.top = parseInt(topo.style.top) - movimento + 'px';
-                break;
-            case 'ArrowDown':
-                topo.style.top = parseInt(topo.style.top) + movimento + 'px';
-                break;
-        }
-    })
+
+
+
 
 
 
