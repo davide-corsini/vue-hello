ae70bb216b01e349b9dc697eb9a4b5e3efb38d17
davide <davidecorsinimilano@gmail.com>
2021-01-26 02:00:12 +0100

    FINAL PUSH DELLE 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt 
 
+
 let move = new Vue({
     el: '#container',
     data: {
         quadrato: false,
         message: false,
         src: '',
-        titolo: 'prova'
+        titolo: 'prova',
+        colori: ['colore1','colore2','colore3','colore4','colore5','colore6','colore7'],
+        numberColor: 0
     },
     methods: {
         addText() {
@@ -16,14 +19,17 @@ let move = new Vue({
         },
         addImg() {
             (this.src == '') ? this.src += 'https://i.ytimg.com/vi/gzc80ics6sI/maxresdefault.jpg' : this.src = '';
+            alert("Non esiste un traguardo!");
+        },
+        getColor(){
+            this.numberColor = Math.floor(Math.random()*7);
         }
-
     }
-})
+});
 
         let topo = document.getElementById('topo');
         console.log(topo);
-        const movimento = 10; //questo 10rappresenta i miei px
+        const movimento = 18; //questo 10rappresenta i miei px
         console.log(movimento);
 
 
@@ -42,25 +48,31 @@ let move = new Vue({
 
             switch (speed.key) {
                 case 'ArrowRight':
+                    topo.style.transform = 'scaleX(-1)';
                     topo.style.right = parseInt(topo.style.right) - movimento + 'px';
                     break;
                 case 'ArrowLeft':
+                    topo.style.transform = 'scaleX(1)';
                     topo.style.right = parseInt(topo.style.right) + movimento + 'px';
                     break;
                 case 'ArrowDown':
+                    topo.style.transform = 'rotate(-90deg)';
                     topo.style.bottom = parseInt(topo.style.bottom) - movimento + 'px';
                     break;
                 case 'ArrowUp':
+                    topo.style.transform = 'rotate(90deg)';
                     topo.style.bottom = parseInt(topo.style.bottom) + movimento + 'px';
                     break;
             }
         });
 
 
-        
+// var colors = ['red', 'white', 'green', 'black', 'orange', 'greenyellow', 'blue'];
+
+// var getColorRandom = colors[Math.floor(Math.random() * colors.length)]
 
 
-
+// console.log(getColorRandom);
 
 
 
